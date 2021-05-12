565859ddb8524ffdd6a4523aabb8a80b76bc8cd1
abhish1Ek <abhish1.mishra@gmail.com>
2021-05-12 13:38:05 +0530

    travis setup

---
+++ tests/factories/sessionFactory.js
@@ -0,0 +1,18 @@
+const Buffer = require("safe-buffer").Buffer;
+const keys = require("../../config/keys");
+const Keygrip = require("Keygrip");
+const keyGrip = new Keygrip([keys.cookieKey]);
+
+module.exports = (user) => {
+  const sessionObject = {
+    passport: {
+      user: user._id.toString(),
+    },
+  };
+
+  const session = Buffer.from(JSON.stringify(sessionObject)).toString("base64");
+
+  const sig = keyGrip.sign("session=" + session);
+
+  return { session, sig };
+};
